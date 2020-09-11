c62dc7aa55763cfa8a4984369c0f8ef1de404f8e
Thiago Schoeffel <thiago@7hs.com.br>
2020-09-11 20:13:39 -0300

    Studying react state and immutability

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    function handleAddProject() {
+        // projects.push(`Novo projeto ${Date.now()}`);
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    }
+
     return (
         <>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
-            <Header title="Projects">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </ul>
-            </Header>
+            <Header title="Projects" />
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     );
 }
