e7fe08da0c0fceebad70c7a28539d9bdf4c50ff9
Thiago Schoeffel <thiago@7hs.com.br>
2020-09-11 21:48:49 -0300

    Add api connection to fron-end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
 
     function handleAddProject() {
         // projects.push(`Novo projeto ${Date.now()}`);
@@ -17,10 +23,8 @@ function App() {
         <>
             <Header title="Projects" />
 
-            <img src={backgroundImage} width={300} />
-
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
